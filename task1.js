d7c2dc90d9b1bb7180c56c812c68309b0425e760
Sreekanth6274 <164642286+Sreekanth6274@users.noreply.github.com>
2024-05-18 11:05:50 +0530

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,154 @@
+// // let academyPeople = ['Sai','Rakesh','Narendra','Niranjan'];
+// // // let length = academyPeople.length;
+// // console.log(academyPeople.length);
+// // console.log(
+// //      academyPeople);
+
+
+//      // let personInfo = {    
+//      //      Name : "Saikumar",
+//      //      lName : "Gadde",
+//      //      a : 35,
+//      //      places : ['Munnar', 'Athirapally', 'Varkala'],
+//      //      family : {
+//      //          fatherName : {
+//      //              fatherFirstName : "King",
+//      //              fLastName : 'Ruler'
+//      //          },
+//      //          motherName : 'Queen'
+//      //      },
+//      //      isHeworking : true,
+//      //      favBikes : ['R15', 'Xpulse']
+//      //  };
+      
+//      //  console.log(personInfo);
+
+
+//      let personDetails = {
+//         Name : "sreekanth",
+//         lName : "Peddarapu",
+//         family : {
+//                       fatherName : {
+//                           fatherFirstName : "King",
+//                           fLastName : 'Ruler'
+//                       },
+//                       motherName : {
+//                        motherFirstName : "Rama",
+//                        mLastName : "Queen"
+//                   }
+//              }
+//              }
+             
+//    console.log(personDetails) ;
+
+
+//   let sri =  personDetails.family
+
+//   console.log( sri);
+
+
+//   let friendsNames = ["vamsi","RAkesh","NArendra"]
+//   console.log(friendsNames);
+
+// // push
+// friendsNames.push("Venkat")
+// console.log(friendsNames)
+
+// // pop
+
+// friendsNames.pop()
+// console.log(friendsNames)
+// friendsNames.pop()
+// console.log(friendsNames)
+
+// // unshift
+
+// friendsNames.unshift("Krishna")
+// console.log(friendsNames)
+
+// let text = "Hello World!";
+// let result = text.replace('h','p');
+// console.log(result)
+
+// // explicity conversation
+
+// let n1 = 10
+// let n2 = "20"
+// n2 = Number(n2)
+// console.log(n1 + n2)
+
+// let van = "Sreekanth"
+// alert(van)
+
+// // let academyPeople = ['Sai','Rakesh','Narendra','Niranjan'];
+// // // let length = academyPeople.length;
+// // console.log(academyPeople.length);
+// // console.log(
+// //      academyPeople);
+
+
+//      // let personInfo = {    
+//      //      Name : "Saikumar",
+//      //      lName : "Gadde",
+//      //      a : 35,
+//      //      places : ['Munnar', 'Athirapally', 'Varkala'],
+//      //      family : {
+//      //          fatherName : {
+//      //              fatherFirstName : "King",
+//      //              fLastName : 'Ruler'
+//      //          },
+//      //          motherName : 'Queen'
+//      //      },
+//      //      isHeworking : true,
+//      //      favBikes : ['R15', 'Xpulse']
+//      //  };
+      
+//      //  console.log(personInfo);
+
+
+// //  s
+
+//   let friendsNames = ["vamsi","RAkesh","NArendra"]
+//   console.log(friendsNames);
+
+// // push
+// friendsNames.push("Venkat")
+// console.log(friendsNames)
+
+// // pop
+
+// friendsNames.pop()
+// console.log(friendsNames)
+// friendsNames.pop()
+// console.log(friendsNames)
+
+// // unshift
+
+// friendsNames.unshift("Krishna")
+// console.log(friendsNames)
+
+// let text = "Hello World!";
+// let result = text.replace('h','p');
+// console.log(result)
+
+// // explicity conversation
+
+// let n1 = 10
+// let n2 = "20"
+// n2 = Number(n2)
+// console.log(n1 + n2)
+
+// let van = "Sreekanth"
+// alert(van)
+
+movie1 = false;
+movie2 = true;
+
+if(movie1){
+     let movie = "went to movie"  
+     console.log(movie1)
+     
+} else if(movie2){
+     let movie2 ='other wise went to college'
+     console.log(movie2)
+}
